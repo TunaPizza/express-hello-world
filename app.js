4135cee1299dcbdf9608d552040b6d790711f13a
TunaPizza <23FI031@ms.dendai.ac.jp>
2025-07-10 13:30:06 +0900

    change app

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ expressWs(app)
 
 const port = process.env.PORT || 3001
 let connects = []
+let users = new Map()
 
 app.use(express.static('public'))
 
@@ -13,7 +14,36 @@ app.ws('/ws', (ws, req) => {
   connects.push(ws)
 
   ws.on('message', (message) => {
+     const msg = JSON.parse(message)
     console.log('Received:', message)
+    
+    if (msg.type === 'join') {
+      const isHost = connects.length === 1
+      users.set(ws, { id: msg.id, isHost })
+
+ // 全員に join 通知を送信
+      connects.forEach((socket) => {
+        if (socket.readyState === 1) {
+          const isSelf = socket === ws
+          socket.send(JSON.stringify({
+            type: 'join',
+            id: msg.id,
+            isHost: isSelf ? isHost : false
+          }))
+        }
+      })
+      return
+    }
+
+    if (msg.type === 'start') {
+      // 全員にゲーム開始通知
+      connects.forEach((socket) => {
+        if (socket.readyState === 1) {
+          socket.send(JSON.stringify({ type: 'start' }))
+        }
+      })
+      return
+    }
 
     connects.forEach((socket) => {
       if (socket.readyState === 1) {
